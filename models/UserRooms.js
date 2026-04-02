0e5dff29d7e3ed38d635fc2591d5fa1366eba721
simezi25253-afk <simezi25253@gmail.com>
2026-04-02 22:49:31 +0900

    Update UserRooms.js

--- models/UserRooms.js
+++ models/UserRooms.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose");
 
 const userRoomsSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  rooms: { type: [String], default: [] }
+
+  // 🔽 ここを拡張：rooms を「文字列の配列」→「オブジェクトの配列」に変更
+  rooms: [
+    {
+      roomId: { type: String, required: true },
+      roomName: { type: String, required: true },
+      joinedAt: { type: Date, default: Date.now }
+    }
+  ]
 });
 
 module.exports = mongoose.model("UserRooms", userRoomsSchema);
